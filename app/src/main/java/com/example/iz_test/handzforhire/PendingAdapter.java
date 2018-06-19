9ead5b123d71c4ee20196b44a03f42ead68f457a
Izaap <baskaran.izaap@gmail.com>
2018-06-19 12:29:17 +0530

    Pending adapter and pending job_xml

--- app/src/main/java/com/example/iz_test/handzforhire/PendingAdapter.java
+++ app/src/main/java/com/example/iz_test/handzforhire/PendingAdapter.java
@@ -1,11 +1,15 @@
 package com.example.iz_test.handzforhire;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,6 +21,7 @@ public class PendingAdapter extends BaseAdapter {
     private Activity activity;
     private ArrayList<HashMap<String, String>> data;
     private static LayoutInflater inflater = null;
+
 
     public PendingAdapter(Activity a, ArrayList<HashMap<String, String>> d) {
         activity = a;
@@ -41,15 +46,31 @@ public class PendingAdapter extends BaseAdapter {
         if (convertView == null)
             vi = inflater.inflate(R.layout.pending_job_list, null);
 
-        TextView job_name = (TextView) vi.findViewById(R.id.job_name_text);
-        TextView date = (TextView) vi.findViewById(R.id.text3);
-        TextView amount = (TextView) vi.findViewById(R.id.text7);
-        TextView type = (TextView) vi.findViewById(R.id.text8);
+        TextView job_name = (TextView) vi.findViewById(R.id.job);
+        TextView job_date = (TextView) vi.findViewById(R.id.whe);
+        TextView pay = (TextView) vi.findViewById(R.id.pay);
+        TextView job_type = (TextView) vi.findViewById(R.id.esti);
         TextView jobId = (TextView) vi.findViewById(R.id.job_id);
+        ImageView img=(ImageView) vi.findViewById(R.id.cir);
+
+        img.setOnClickListener(new View.OnClickListener()
+        {
+        @Override
+         public void onClick(View v)
+        {
+
+
+
+
+
+
+     }
+    });
+
 
         HashMap<String, String> items = new HashMap<String, String>();
         items = data.get(position);
-        final String get_name = items.get("name");
+        /*final String get_name = items.get("name");
         System.out.println("iiiiiiiiiiiiiiiiiiid:get_name::" + get_name);
         final String get_date = items.get("date");
         System.out.println("iiiiiiiiiiiiiiiiiiid:get_date::" + get_date);
@@ -58,13 +79,27 @@ public class PendingAdapter extends BaseAdapter {
         String get_type = items.get("type");
         System.out.println("iiiiiiiiiiiiiiiiiiid:get_recur::" + get_type);
         String get_id = items.get("jobId");
-        System.out.println("iiiiiiiiiiiiiiiiiiid:get_id::" + get_id);
+        System.out.println("iiiiiiiiiiiiiiiiiiid:get_id::" + get_id);*/
+        final String get_jobname=items.get("name");
+        System.out.println("1111111"+get_jobname);
+        final String get_jobdate=items.get("date");
+        System.out.println("2222222"+get_jobdate);
+        final String get_pay=items.get("amount");
+        System.out.println("3333333"+get_pay);
+        final String get_esti=items.get("type");
+        System.out.println("4444444"+get_esti);
+        final String get_status=items.get("status");
+        System.out.println("555555"+get_status);
 
-        job_name.setText(get_name);
-        date.setText(get_date);
-        amount.setText(get_amount);
-        type.setText(get_type);
-        jobId.setText(get_id);
+
+        //job_name.setText("PAY"+get_jobname);
+
+
+        job_name.setText(""+get_jobname);
+        job_date.setText("WHEN:"+get_jobdate);
+        pay.setText("PAY:"+get_pay);
+        job_type.setText("ESTIMATED DURATION:"+get_esti);
+        //jobId.setText(get_id);
 
         return vi;
     }
